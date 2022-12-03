ee8e9e1c75e75472e8cddc0d18f20dd573544db4
Anurag Soni <anuragsoni39@yahoo.com>
2022-12-03 08:13:46 +0000

    Breakdown logic into methods

--- 01_01b/script.js
+++ 01_01b/script.js
@@ -9,24 +9,43 @@ import data from "./data.js";
 
 const mainContent = document.querySelector(".main-content");
 
+const getNiceDate = (imgData) => {
+  const date = Date(imgData.created_at);
+  const niceDate = date.toLocaleString("en-IN", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  return niceDate;
+};
+
+const getImageData = (imgData) => {
+  let srcset = `${imgData.urls.full} ${imgData.width}w`;
+  if (imgData.urls.regular) {
+    srcset = srcset + `,${imgData.urls.regular} 1080w`;
+  }
+  if (imgData.urls.small) {
+    srcset = srcset + `,${imgData.urls.small} 400w`;
+  }
+
+  const data = `<img
+      srcset="${srcset}"
+      sizes="(max-width: 450px) 400px, (max-width: 800) 1080px"
+      src="${imgData.urls.regular}"
+      width="${imgData.width}"
+      height="${imgData.height}"
+      alt="${imgData.description}"
+      loading="lazy"
+    />`;
+  return data;
+};
+
 const Card = (data) => {
   const imgData = data[0];
-  const date = Date(imgData.created_at);
+  // const date = Date(imgData.created_at);
   const markup = `
     <figure class="image">
-      <img
-        srcset="
-          ${imgData.urls.full} ${imgData.width}w,
-          ${imgData.urls.regular} 1080w,
-          ${imgData.urls.small} 400w
-        "
-        sizes="(max-width: 450px) 400px, (max-width: 800) 1080px"
-        src="${imgData.urls.regular}"
-        width="${imgData.width}"
-        height="${imgData.height}"
-        alt="${imgData.description}"
-        loading="lazy"
-      />
+      ${getImageData(imgData)}
       <figcaption class="image__caption">
         <h3 class="image__title">${imgData.description}</h3>
         <div class="image__meta">
@@ -37,11 +56,7 @@ const Card = (data) => {
           <p>
             Created on
             <time class="image__createdon" datetime="${imgData.created_at}">
-            ${date.toLocaleString("en-IN", {
-              year: "numeric",
-              month: "long",
-              day: "numeric",
-            })}</time>.
+            ${getNiceDate(imgData)}</time>.
           </p>
           <p>
             <a href="${imgData.links.self}" class="image__link">
